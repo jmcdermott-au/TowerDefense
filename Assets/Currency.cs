b02f4b69fbbb5cdf1b76439ddf39acdf28741d7d
jmcdermott-au <63567692+jmcdermott-au@users.noreply.github.com>
2023-06-16 13:36:00 +1000

    finishing touches

--- Assets/Currency.cs
+++ Assets/Currency.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class Currency : MonoBehaviour
 {
     public static int money;
     private int startMoney = 500;
+    public TextMeshProUGUI textMeshPro;
 
     private void Start()
     {
         money = startMoney;
 
+    }
+
+    public void FixedUpdate()
+    {
+        textMeshPro.text = money.ToString() + "$";
     }
 }
